76dcb6f19aa51d6dec42c85570bfb2437cd62c47
nbits.y <nbits.y@gmail.com>
2011-06-09 20:28:15 +0800

    add acupra table

---
+++ Core/GData/AcuPraTable.h
@@ -0,0 +1,32 @@
+
+#ifndef ACUPRA_H_
+#define ACUPRA_H_
+
+#include "Config.h"
+#include "ObjectManager.h"
+
+namespace GData
+{
+
+struct AcuPra : public ObjectBaseNT<UInt16>
+{
+    AcuPra(const UInt16& id) : ObjectBaseNT<UInt16>(id) {}
+
+    UInt8 needlvl;
+    UInt32 pra;
+    UInt32 soulmax;
+    UInt32 pramax;
+    UInt32 citslot;
+    UInt8 aura;
+    UInt8 auraInc;
+};
+
+typedef ObjectMapT<AcuPra, UInt16> AcuPraManager;
+#define acupraManager AcuPraManager::Instance()
+
+}
+
+#endif // ACUPRA_H_
+
+/* vim: set ai si nu sm smd hls is ts=4 sm=4 bs=indent,eol,start */
+
