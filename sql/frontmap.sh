50fab5bb7aefebfa03b03c24d5ed8138e5df81c9
nbits.y <nbits.y@gmail.com>
2011-08-15 17:08:39 -0400

    更新阵图配置

---
+++ sql/frontmap.sh
@@ -0,0 +1,57 @@
+
+#!/bin/bash
+
+F=frontmap.txt
+if [ "$1" != "" ]
+then
+    F=$1
+fi
+
+function frontmap()
+{
+    f=$1
+    d=frontmap
+    sed -i /isPack/d $f
+    sed -i /ID/d $f
+    sed -i /^$/d $f
+    sed -i s/\"//g $f
+    export lines=`wc -l $f | awk '{print $1}'`
+    echo "Generating file $d, total lines $l"
+    awk '
+        BEGIN {
+            print "INSERT INTO `frontmap` VALUES";
+        } {
+            printf("(%d,%d,%d,%d)",$1,$2,$3,$4);
+            if (NR <= ENVIRON["lines"]-1)
+                printf(",");
+            else if (NR >= ENVIRON["lines"])
+                printf(";");
+            printf("\n");
+        }
+        END {
+        }
+    ' $f > $d
+    sed -i s/\\r//g $d
+    if [ $? -eq 0 ]
+    then
+        iconv2utf8 $d
+        echo "OK"
+    else
+        echo "ERROR"
+    fi
+}
+
+function iconv2utf8()
+{
+    iconv -f cp936 -t utf8 $1 -o $1.tmp
+    rm $1
+    mv $1.tmp $1
+}
+
+if [ -f $F  ]
+then
+    frontmap $F
+else
+    echo "File $F is not exists"
+fi
+
