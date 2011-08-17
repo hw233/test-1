379ceabb01552654b80e253c6304cbb3971821d6
nbits.y <nbits.y@gmail.com>
2011-08-17 14:57:58 -0400

    i'm sorry

--- Core/GObject/Mail.cpp
+++ Core/GObject/Mail.cpp
@@ -198,7 +198,7 @@ MailPackage * MailPackageManager::add( UInt32 id )
 
 Mail * MailBox::newMail( Player * sender, UInt8 type, const std::string& title, const std::string& content, UInt32 additional, bool writedb, MailItemsInfo * itemsInfo)
 {
-    if (type != 3)
+    if (type != 3 && type != 0x13 && type != 0x80)
         return NULL; // TODO:
 	Mutex::ScopedLock lk(_owner->getMutex());
 
