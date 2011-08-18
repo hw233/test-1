fe0068416b545aab204a489a813a54afe4b223c6
nbits.y <nbits.y@gmail.com>
2011-08-18 14:20:28 -0400

    xxx

--- sql/dbv.sh
+++ sql/dbv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source conf.sh
-mysql -h$H -u$U -p$P -P$PT $DBO -e "replace into database_version values (`date +%Y%m%d`)";
+mysql -h$H -u$U -p$P -P$PT $DBO -e "delete from database_version; insert into database_version values (`date +%Y%m%d`)";
