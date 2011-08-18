c4b65e42e9244d6818e050a65bad3522a6805192
nbits.y <nbits.y@gmail.com>
2011-08-17 20:10:55 -0400

    数据更新

--- sql/clear.sh
+++ sql/clear.sh
@@ -2,5 +2,5 @@
 
 source conf.sh
 mysql -h$H -u$U -p$P -P$PT -e "drop database $DBD; drop database $DBO;"
-mysql -h$H -u$U -p$P -P$PT -e "create database $DBD;create database $DBO;"
+mysql -h$H -u$U -p$P -P$PT -e "set names utf8; create database $DBD;create database $DBO;"
 
