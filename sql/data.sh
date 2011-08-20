eb91feee3315085f05e5662e9df0975c4f4e497e
nbits.y <nbits.y@gmail.com>
2011-08-20 14:12:47 -0400

    数据更新

--- sql/data.sh
+++ sql/data.sh
@@ -5,4 +5,8 @@ mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from map_object; source mapobject;";
 mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from item_template; source itemtemplate;";
 mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from npc_group; source npcgroup;";
 mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from dungeon_level; source dungeonlevel;";
+mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from acupra; source acupra;";
+mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from dungeon_monster; source dungeonmonster;";
+mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from lvl_exp; source lvlexp;";
+mysql -h$H -u$U -p$P -P$PT $DBD -e "delete from special_fighter_template; source npc;";
 
