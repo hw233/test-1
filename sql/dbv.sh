2cc3d75dc80e30e3fb0a5c1e3bc9a0c8ba62802c
nbits.y <nbits.y@gmail.com>
2011-08-23 18:26:01 -0400

    xxx

--- sql/dbv.sh
+++ sql/dbv.sh
@@ -2,4 +2,4 @@
 source conf.sh
 d=`date +%Y%m%d`
 #mysql -h$H -u$U -p$P -P$PT $DBO -e "delete from database_version; insert into database_version values ($d)";
-mysql -h$H -u$U -p$P -P$PT $DBO -e "delete from database_version; insert into database_version values ('20110822')";
+mysql -h$H -u$U -p$P -P$PT $DBO -e "delete from database_version; insert into database_version values ('201108')";
