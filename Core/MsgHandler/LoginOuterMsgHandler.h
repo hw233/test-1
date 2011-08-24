18bdf02eef7d0e69d90aed84ee921d8bf7429770
nbits.y <nbits.y@gmail.com>
2011-08-24 15:44:45 -0400

    xxx

--- Core/MsgHandler/LoginOuterMsgHandler.h
+++ Core/MsgHandler/LoginOuterMsgHandler.h
@@ -438,7 +438,7 @@ void NewUserReq( LoginMsgHdr& hdr, NewUserStruct& nu )
 			}
 
             pl->GetPackage()->AddItem(18, 1, true);
-            {
+            { // TODO:
                 gold = 1000;
 				GameMsgHdr hdr(0x2F0, country, pl, sizeof(UInt32));
 				GLOBAL().PushMsg(hdr, &gold);
