a0510081cd025730dbd304d6e0c70a97aebb684d
nbits.y <nbits.y@gmail.com>
2011-08-28 05:37:30 -0400

    数据更新

--- sql/object.sh
+++ sql/object.sh
@@ -2,8 +2,8 @@
 source conf.sh
 function update()
 {
-    mysql -h$H -u$U -p$P -P$PT -D$DBO < updates/Object_20110825_1.sql
     mysql -h$H -u$U -p$P -P$PT -D$DBO < updates/Object_20110826_1.sql
+    mysql -h$H -u$U -p$P -P$PT -D$DBO < updates/Object_20110825_1.sql
 }
 
 selectAction $DBO
