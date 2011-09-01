fbc4ba67905735716a8d8c2917385b3d649e7245
nbits.y <nbits.y@gmail.com>
2011-09-01 11:09:50 -0400

    xxx

---
+++ Core/GObject/WorldBoss.cpp
@@ -0,0 +1,177 @@
+
+#include "WorldBoss.h"
+#include "Map.h"
+#include "Common/URandom.h"
+#include "Server/SysMsg.h"
+#include "Fighter.h"
+#include "GData/NpcGroup.h"
+#include "Server/Cfg.h"
+
+namespace GObject
+{
+    static UInt32 worldboss[] = {5162, 5103, 5168, 5127, 5197, 5164};
+
+    bool WorldBoss::isWorldBoss(UInt32 npcid)
+    {
+        for (UInt8 i = 0; i < 6; ++i)
+        {
+            if (worldboss[i] == npcid)
+                return true;
+        }
+        return false;
+    }
+
+    void WorldBoss::refresh(UInt32 now)
+    {
+        time_t now2 = static_cast<time_t>(now);
+        struct tm * t = localtime(&now2);
+
+        UInt8 level = 0;
+        if (t->tm_hour >= 18)
+        {
+            level = 6;
+        }
+        else if (t->tm_hour >= 17)
+        {
+            level = 5;
+        }
+        else if (t->tm_hour >= 16)
+        {
+            level = 4;
+        }
+        else if (t->tm_hour >= 15)
+        {
+            level = 3;
+        }
+        else if (t->tm_hour >= 14)
+        {
+            level = 2;
+        }
+        else if (t->tm_hour >= 13)
+        {
+            level = 1;
+        }
+
+        if (!cfg.GMCheck)
+            level = uRand(6)+1;
+
+        if (!level)
+            return;
+
+        reset();
+
+        std::vector<UInt16> spots;
+        Map::GetAllSpot(spots);
+        if (!spots.size())
+            return;
+
+        UInt16 spot = spots[uRand(spots.size())];
+        if (!spot)
+            return;
+
+        UInt32 npcid = worldboss[level-1];
+        Fighter* fgt = globalFighters[npcid];
+        if (fgt)
+        {
+            GData::NpcGroup* ng = GData::npcGroups[npcid];
+            if (ng)
+            {
+                Map * map = Map::FromSpot(spot);
+                if (map)
+                {
+                    MOData mo;
+                    mo.m_ID = npcid;
+                    mo.m_Hide = false;
+                    mo.m_Spot = spot;
+                    mo.m_Type = 6;
+                    mo.m_ActionType = 0;
+                    map->AddObject(mo);
+
+                    if (cfg.GMCheck)
+                    {
+                        SYSMSG_BROADCAST(548);
+                    }
+                    else
+                    {
+                        SpotData* sd = Map::Spot(spot);
+                        if (sd)
+                        {
+                            SYSMSG_BROADCAST(548);
+                            SYSMSG_BROADCASTV(549, sd->m_Name.c_str());
+                        }
+                    }
+
+                    DB().PushUpdateData("REPLACE INTO `worldboss` (`npcId`, `level`, `location`, `count`) VALUES (%u,%u,%u,%u)", npcid, level, spot, 0);
+                }
+            }
+        }
+    }
+
+    void WorldBoss::prepare(UInt32 now)
+    {
+        if (!cfg.GMCheck)
+            SYSMSG_BROADCAST(547);
+
+        time_t now2 = static_cast<time_t>(now);
+        struct tm * t = localtime(&now2);
+        if ((t->tm_hour >= 12 && t->tm_min >= 45 && t->tm_hour < 13) ||
+            (t->tm_hour >= 13 && t->tm_min >= 45 && t->tm_hour < 14) ||
+            (t->tm_hour >= 14 && t->tm_min >= 45 && t->tm_hour < 15) ||
+            (t->tm_hour >= 15 && t->tm_min >= 45 && t->tm_hour < 16) ||
+            (t->tm_hour >= 16 && t->tm_min >= 45 && t->tm_hour < 17) ||
+            (t->tm_hour >= 17 && t->tm_min >= 45 && t->tm_hour < 18))
+        {
+            SYSMSG_BROADCAST(547);
+            return;
+        }
+    }
+
+    void WorldBoss::attack(Player* pl, UInt16 loc, UInt32 npcid)
+    {
+        std::map<UInt16, WBoss>::iterator i = m_boss.find(loc);
+        if (i != m_boss.end())
+        {
+            if (i->second.npcId == npcid)
+            {
+                if (i->second.count >= 9)
+                {
+                    SYSMSG_SEND(550, pl);
+                    return;
+                }
+
+                if (pl->attackNpc(npcid))
+                {
+                    ++i->second.count;
+                    DB().PushUpdateData("UPDATE `worldboss` SET `count` = %u WHERE `npcId` = %u", i->second.count, npcid);
+                }
+            }
+        }
+    }
+
+    void WorldBoss::reset()
+    {
+        for (std::map<UInt16, WBoss>::iterator i = m_boss.begin(), e = m_boss.end(); i != e; ++i)
+        {
+            Map * map = Map::FromSpot(i->first);
+            if (map)
+            {
+                map->DelObject(i->second.npcId);
+            }
+        }
+
+        m_boss.clear();
+        DB().PushUpdateData("DELETE FROM `worldboss`");
+    }
+
+    void WorldBoss::add(UInt16 loc, UInt32 npcId, UInt8 level, UInt8 count)
+    {
+        WBoss wb;
+        wb.npcId = npcId;
+        wb.level = level;
+        wb.count = count;
+        m_boss[loc] = wb;
+    }
+
+    WorldBoss worldBoss;
+}
+
---
+++ Core/GObject/WorldBoss.h
@@ -0,0 +1,46 @@
+
+#ifndef WORLDBOSS_H_
+#define WORLDBOSS_H_
+
+#include "Config.h"
+
+namespace GObject
+{
+
+class Player;
+
+struct WBoss
+{
+    UInt32 npcId;
+    UInt8 level;
+    UInt8 count;
+};
+
+class WorldBoss
+{
+public:
+    static bool isWorldBoss(UInt32 npcid);
+
+public:
+    WorldBoss() : m_max(1) {}
+    ~WorldBoss() {}
+
+    void refresh(UInt32);
+    void prepare(UInt32 now);
+    void attack(Player* pl, UInt16 loc, UInt32 npcid);
+    void reset();
+    const WBoss& get(UInt16 loc) { return m_boss[loc]; }
+    void add(UInt16 loc, UInt32 npcId, UInt8 level, UInt8 count);
+private:
+    std::map<UInt16, WBoss> m_boss;
+    UInt8 m_max;
+};
+
+extern WorldBoss worldBoss;
+
+}
+
+#endif // WORLDBOSS_H_
+
+/* vim: set ai si nu sm smd hls is ts=4 sm=4 bs=indent,eol,start */
+
