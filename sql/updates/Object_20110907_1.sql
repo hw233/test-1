4957e2e3c42057d35f5d1ce2fa0380ae4c935900
nbits.y <nbits.y@gmail.com>
2011-09-07 20:38:50 -0400

    xxx

--- sql/updates/Object_20110907_1.sql
+++ sql/updates/Object_20110907_1.sql
@@ -1,2 +1,8 @@
 alter table `player` add column `qqvipl` tinyint(3) unsigned NOT NULL DEFAULT '0' after `archievement`;
 alter table `player` add column `qqvipyear` tinyint(3) unsigned NOT NULL DEFAULT '0' after `archievement`;
+
+
+alter table `athletics_rank` modify column `extrachallenge` int(10) unsigned NOT NULL DEFAULT '0';
+
+alter table `practice_data` add column `hookadd` tinyint(2) unsigned NOT NULL DEFAULT '0' after `winnerid`;
+alter table `practice_data` add column `nexthareset` int(10) unsigned NOT NULL DEFAULT '0' after `hookadd`;
