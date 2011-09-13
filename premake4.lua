54db0556f70a4fbc097ec3aa8d0be1ee48d93225
nbits.y <nbits.y@gmail.com>
2011-09-13 10:26:12 -0400

    memcached

--- premake4.lua
+++ premake4.lua
@@ -31,8 +31,8 @@ solution "HoHH"
         libdirs { "/opt/local/lib" }
 
     configuration "linux"
-    	includedirs { "/usr/include/mysql", "/usr/local/include/mysql", "/usr/local/include", "/usr/local/mysql", "/usr/local/mysql/include", "/usr/local/mysql/include/mysql", "/usr/local/services/mysql/include/mysql", "/usr/local/services/libevent/include", "tools/udplog_cpp" }
-        libdirs { "/usr/local/lib/mysql", "/usr/local/lib64", "/usr/local/lib", "/usr/local/mysql/lib", "/usr/local/mysql/lib/mysql/", "/usr/local/services/mysql/lib/mysql", "/usr/local/services/libevent/lib", "tools/udplog_cpp" }
+    	includedirs { "/usr/include/mysql", "/usr/local/include/mysql", "/usr/local/include", "/usr/local/mysql", "/usr/local/mysql/include", "/usr/local/mysql/include/mysql", "/usr/local/services/mysql/include/mysql", "/usr/local/services/libevent/include", "tools/udplog_cpp", "/usr/local/services/libmemcached/include" }
+        libdirs { "/usr/local/lib/mysql", "/usr/local/lib64", "/usr/local/lib", "/usr/local/mysql/lib", "/usr/local/mysql/lib/mysql/", "/usr/local/services/mysql/lib/mysql", "/usr/local/services/libevent/lib", "tools/udplog_cpp", "/usr/local/services/libmemcached/lib" }
 
     configuration "vs*"
 
