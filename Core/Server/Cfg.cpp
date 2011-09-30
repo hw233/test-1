de07c5cea5baf4ac2efd62f4a857efa3be4c0f98
nbits.y <nbits.y@gmail.com>
2011-09-29 22:04:28 -0400

    xx

--- Core/Server/Cfg.cpp
+++ Core/Server/Cfg.cpp
@@ -7,7 +7,7 @@
 Cfg::Cfg( ): tcpPort(8888), serverLogId(0), dbDataPort(3306), dbObjectPort(3306),
 	openYear(2011), openMonth(9), openDay(23), enableWallow(false), limitLuckyDraw(0),
 	merged(false), supportCompress(true), GMCheck(true), channelNum(0), serverNum(0), arenaPort(0),
-	_filename("conf/config.lua"), enableLoginLimit(false), loginLimit(10000), onlineLimit(0)
+	enableLoginLimit(false), loginLimit(10000), onlineLimit(0), _filename("conf/config.lua")
 {
 }
 
