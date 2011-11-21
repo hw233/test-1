ae9e728bb5764e6382593c3d77e2a38c79827822
nbits.y <nbits.y@gmail.com>
2011-11-21 19:06:22 +0800

    xxx

--- Core/GObject/HeroIsland.cpp
+++ Core/GObject/HeroIsland.cpp
@@ -1446,6 +1446,7 @@ void HeroIsland::playerLeave(Player* player)
     HIPlayerData* pd = findPlayer(player, spot, pos);
     if (!pd) return;
 
+#if 0
     if (spot)
     {
         pd = leave(pd, spot, pos);
@@ -1461,6 +1462,10 @@ void HeroIsland::playerLeave(Player* player)
         if (_nplayers[0])
             --_nplayers[0];
     }
+#else
+    if (_nplayers[spot])
+        --_nplayers[spot];
+#endif
 
     clearBuff(1, pd, 0);
     clearBuff(2, pd, 0); // XXX: must before reset
@@ -1488,6 +1493,9 @@ void HeroIsland::playerOffline(Player* player)
     HIPlayerData* pd = findPlayer(player, spot, pos);
     if (!pd) return;
 
+    Stream st(REP::HERO_ISLAND);
+    st << static_cast<UInt8>(7) << static_cast<UInt8>(1) << pd->player->getId() << Stream::eos;
+    broadcast(st, pd->spot);
     if (_nplayers[spot])
         --_nplayers[spot];
 }
