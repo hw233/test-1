e712ed691f83e19aea7ecd585b6ab8ca5112b39c
nbits.y <nbits.y@gmail.com>
2012-05-04 11:40:27 +0800

    xxx

--- sql/object.sh
+++ sql/object.sh
@@ -44,7 +44,7 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120417_1.sql # must be once
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120423_1.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120427_1.sql
-    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120503_1.sql
+    #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120503_1.sql # must be once
     #. clearshiyamen.sh
 }
 
