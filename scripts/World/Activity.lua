05f9ac4e7e4a8be5146e3c430f21f4c9842a90d7
nbits.y <nbits.y@gmail.com>
2012-05-22 18:28:46 +0800

    recharge activety

--- scripts/World/Activity.lua
+++ scripts/World/Activity.lua
@@ -196,12 +196,6 @@ function onActivityCheck(tm)
       is_4_21 = false
   end
 
-  if tm >= actTime47 and tm < actTime48 then
-      setRechargeActive(true, 4)
-  else
-      setRechargeActive(false, 4)
-  end
-
   if isFBVersion() then
       if tm >= actTime101 and tm < actTime102 then
           setFighter1368(true)
@@ -231,6 +225,12 @@ function onActivityCheck(tm)
           setNetValentineDay(true)
       else
           setNetValentineDay(false)
+      end
+
+      if tm >= actTime111 and tm < actTime112 then
+          setRechargeActive(true, 4)
+      else
+          setRechargeActive(false, 4)
       end
 
       setShuoShuo(false);
@@ -275,6 +275,12 @@ function onActivityCheck(tm)
           setQQGameAct(true)
       else
           setQQGameAct(false)
+      end
+
+      if tm >= actTime47 and tm < actTime48 then
+          setRechargeActive(true, 4)
+      else
+          setRechargeActive(false, 4)
       end
 
       setShuoShuo(true);
@@ -329,6 +335,8 @@ function initActTime(y, m, d)
   local  SerStartTm102 = { ['year'] = 2012, ['month'] = 5, ['day'] = 5, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   local  SerStartTm103 = { ['year'] = 2012, ['month'] = 5, ['day'] = 11, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   local  SerStartTm104 = { ['year'] = 2012, ['month'] = 5, ['day'] = 19, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
+  -- FB充值返利（5/23-5/27）
+  local  SerStartTm105= { ['year'] = 2012, ['month'] = 5, ['day'] = 16, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
 
   actTime0 = os.time(SerStartTm);
   actTime00 = os.time(SerStartTm) + 7 * 86400;
@@ -403,7 +411,7 @@ function initActTime(y, m, d)
   actTime46 = os.time(SerStartTm21) + 7 * 86400;
 
   actTime47 = os.time(SerStartTm22);
-  actTime48 = os.time(SerStartTm22) + 11 * 86400;
+  actTime48 = os.time(SerStartTm22) + 12 * 86400;
 
   actTime101 = os.time(SerStartTm101);
   actTime102 = os.time(SerStartTm101) + 8 * 86400;
@@ -419,6 +427,9 @@ function initActTime(y, m, d)
 
   actTime109 = os.time(SerStartTm104);
   actTime110 = os.time(SerStartTm104) + 7 * 86400;
+
+  actTime111 = os.time(SerStartTm105);
+  actTime112 = os.time(SerStartTm105) + 12 * 86400;
 
   onActivityCheck(os.time() + 30);
 
