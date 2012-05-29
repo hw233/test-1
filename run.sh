6d59aad1e632df64eabecedc31000e83be994959
zhgc <zhgc1983@gmail.com>
2012-05-29 20:34:19 +0800

    json proxy

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ do
     cd sql && if [ -f once.sh ]; then . once.sh; rm -f once.sh; fi && cd -
     if [ -f bin/Release/Server.$ID ]; then mv -f bin/Release/Server.$ID bin/Release/Server.$ID.bak; fi
     if [ -f bin/Release/Server.ASSS ]; then mv -f bin/Release/Server.ASSS bin/Release/Server.$ID; fi
+    if [ -f bin/Release/JsonProxy.ASSS ]; then bin/Release/JsonProxy.ASSS; fi
     bin/Release/Server.$ID
     sleep 3
 done
