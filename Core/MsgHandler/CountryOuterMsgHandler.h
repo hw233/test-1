faa7e33135d4a3e85b248b460421b8f7cb22db2f
qiweiyou <qiweiyou1982@163.com>
2012-06-02 00:00:16 +0800

    回流用户奖励

--- Core/MsgHandler/CountryOuterMsgHandler.h
+++ Core/MsgHandler/CountryOuterMsgHandler.h
@@ -1057,71 +1057,62 @@ void OnPlayerInfoReq( GameMsgHdr& hdr, PlayerInfoReq& )
         {
             Stream st(REP::OFFLINEEXP);
             st << pl->GetVar(VAR_OFFLINE_EXP) << static_cast<UInt32>(pl->GetVar(VAR_OFFLINE_PEXP)*pl->getMainFighter()->getPracticeInc()*0.8f);
-#if 0
+#ifndef _FB
             UInt32 equip = pl->GetVar(VAR_OFFLINE_EQUIP);
             if(equip)
             {
-                UInt8 dayCnt = static_cast<UInt8>(equip / (24 * 3600));
+                UInt32 dayCnt = equip / (24 * 3600);
                 UInt8 lvl = pl->GetLev();
-                UInt16 equipId = 0;
-                UInt16 needCnt = 0;
+                GData::LootResult lr;
 
+                pl->_equipAward.clear();
                 if(dayCnt > 365)
-                    needCnt = 365;
-                if(dayCnt < 7)
-                    needCnt = 0;
-                else if(dayCnt < 14)
-                    needCnt = 1;
-                else if(dayCnt < 21)
-                    needCnt = 3;
-                else if(dayCnt < 30)
-                    needCnt = 5;
-                else
-                    needCnt = 7 + (dayCnt - 30) * 2;
-                if (pl->GetPackage()->GetRestPackageSize() < needCnt)
-                {
-                    pl->sendMsgCode(0, 1011);
-                    return;
-                }
-
-                st << needCnt;
+                    dayCnt = 365;
                 if(dayCnt >= 7)
                 {
-                    equipId = getRandOEquip(lvl);
-                    pl->GetPackage()->AddItem(equipId, 1, true);
-                    st << equipId;
+                    lr.id = getRandOEquip(lvl);
+                    lr.count = 1;
+                    pl->_equipAward.push_back(lr);
                 }
                 if(dayCnt >= 14)
                 {
-                    equipId = getRandOEquip(lvl);
-                    pl->GetPackage()->AddItem(equipId, 1, true);
-                    st << equipId;
-                    equipId = GameAction()->getRandTrump(lvl);
-                    pl->GetPackage()->AddItem(equipId, 1, true);
-                    st << equipId;
+                    lr.id = getRandOEquip(lvl);
+                    lr.count = 1;
+                    pl->_equipAward.push_back(lr);
+                    lr.id = GameAction()->getRandTrump(lvl);
+                    lr.count = 1;
+                    pl->_equipAward.push_back(lr);
                 }
                 if(dayCnt >= 21)
                 {
-                    equipId = getRandOEquip(lvl);
-                    pl->GetPackage()->AddItem(equipId, 1, true);
-                    st << equipId;
-                    equipId = GameAction()->getRandTrump(lvl);
-                    pl->GetPackage()->AddItem(equipId, 1, true);
-                    st << equipId;
+                    lr.id = getRandOEquip(lvl);
+                    lr.count = 1;
+                    pl->_equipAward.push_back(lr);
+                    lr.id = GameAction()->getRandTrump(lvl);
+                    lr.count = 1;
+                    pl->_equipAward.push_back(lr);
                 }
-                int times = dayCnt / 30;
-                while(times--)
+                if(dayCnt >= 30)
                 {
-                    equipId = getRandOEquip(lvl);
-                    pl->GetPackage()->AddItem(equipId, 1, true);
-                    st << equipId;
-                    equipId = GameAction()->getRandTrump(lvl);
-                    pl->GetPackage()->AddItem(equipId, 1, true);
-                    st << equipId;
+                    int times = dayCnt / 30;
+                    while(times--)
+                    {
+                        lr.id = getRandOEquip(lvl);
+                        lr.count = 1;
+                        pl->_equipAward.push_back(lr);
+                        lr.id = GameAction()->getRandTrump(lvl);
+                        lr.count = 1;
+                        pl->_equipAward.push_back(lr);
+                    }
                 }
-                pl->SetVar(VAR_OFFLINE_EQUIP, 0);
-            }
+                st << static_cast<UInt8>(pl->_equipAward.size());
+                for(UInt16 i = 0; i < pl->_equipAward.size(); i++)
+                {
+                    st << pl->_equipAward[i].id;
+                }
 #endif
+            }
+
             st<< Stream::eos;
             pl->send(st);
         }
