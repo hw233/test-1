b256954fcb1ad5ec4e1f4a24a0079e24762dc2ed
nbits.y <nbits.y@gmail.com>
2012-06-22 08:39:43 +0800

    7日lua

--- scripts/items/Awards.lua
+++ scripts/items/Awards.lua
@@ -95,37 +95,25 @@ function RunTargetAward(player)
 		return 0;
 	end
 
-    local itemId = 0;
-    local chance = {379, 1895, 2400, 5768, 9558, 9991, 9997, 10000}
+    if player:hasRealItemAward(1) then
+        player:getRealItemAward(1)
+        Broadcast(0x27, "恭喜[p:"..player:getCountry()..":"..player:getPName().."]在七日目标实物抽奖中获得了60QB大奖，让我们一起来祝贺他。玩蜀山传奇，下一个幸运儿就是你！！！！！！！")
+        return 7;
+    end
+    if player:hasRealItemAward(2) then
+        player:getRealItemAward(2)
+        Broadcast(0x27, "恭喜[p:"..player:getCountry()..":"..player:getPName().."]在七日目标实物抽奖中获得了100QB大奖，让我们一起来祝贺他。玩蜀山传奇，下一个幸运儿就是你！！！！！！！")
+        return 8;
+    end
+
+    local chance = {379, 1895, 2400, 5768, 9558, 10000, 10000, 10000}
     local item = {515, 503, 507, 56, 57, 15, 60, 100}
-    local g = math.random(1, 10000)
     local j = 0;
+    local g = math.random(1, 10000)
     for i = 1, #chance do
         if g <= chance[i] then
-            if i <= 6 then
-                player:lastLootPush(item[i], 1);
-                package:AddItem(item[i], 1, true, true, 41);
-            else
-                if i == 7 then
-                    if player:hasRealItemAward(1) then
-                        player:getRealItemAward(1)
-                        Broadcast(0x27, "恭喜[p:"..player:getCountry()..":"..player:getPName().."]在七日目标实物抽奖中获得了60QB大奖，让我们一起来祝贺他。玩蜀山传奇，下一个幸运儿就是你！！！！！！！")
-                    else
-                        i = 1;
-                        player:lastLootPush(item[i], 1);
-                        package:AddItem(item[i], 1, true, true, 41);
-                    end
-                else
-                     if player:hasRealItemAward(2) then
-                         player:getRealItemAward(2)
-                         Broadcast(0x27, "恭喜[p:"..player:getCountry()..":"..player:getPName().."]在七日目标实物抽奖中获得了100QB大奖，让我们一起来祝贺他。玩蜀山传奇，下一个幸运儿就是你！！！！！！！")
-                     else
-                         i = 1;
-                         player:lastLootPush(item[i], 1);
-                         package:AddItem(item[i], 1, true, true, 41);
-                     end
-                end
-            end
+            player:lastLootPush(item[i], 1);
+            package:AddItem(item[i], 1, true, true, 41);
             j = i;
             break
         end
