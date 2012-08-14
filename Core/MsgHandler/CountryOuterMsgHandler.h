0df51c9ca6e6398aa36e4214d6fa87e82cfe4d1e
nbits.y <nbits.y@gmail.com>
2012-08-14 10:23:49 +0800

    数据更新

--- Core/MsgHandler/CountryOuterMsgHandler.h
+++ Core/MsgHandler/CountryOuterMsgHandler.h
@@ -3352,6 +3352,9 @@ void OnSaleSellReq( GameMsgHdr& hdr, SaleSellReq& req )
 	if(!player->hasChecked())
 		return;
 
+    if (player->GetLev() < 45)
+        return;
+
 	player->GetSale()->sellSaleReq(req._data);
 }
 
