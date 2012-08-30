bfd6a868587161b2a3f21b9afb880d4dab8cd7ed
jianglutian <jianglutian@gmail.com>
2012-08-30 13:58:56 +0800

    clanCopy (checkout to discountBugFix)

--- Core/GObject/ClanCopy.cpp
+++ Core/GObject/ClanCopy.cpp
@@ -16,6 +16,7 @@ ClanCopy::ClanCopy(Clan *c) : _clan(c), _copyLevel(0)
     _spotPlayer.clear();
     _spotList.clear();
     _copyProcessTime = 0;
+    _deadPlayer.clear();
 }
 
 
@@ -25,6 +26,23 @@ ClanCopy::~ClanCopy()
     _spotPlayer.clear();
     _spotList.clear();
     _copyProcessTime = 0;
+    _deadPlayer.clear();
+}
+
+void ClanCopy::setInterval(UInt32 interval)
+{
+    // TODO: 设置游戏节奏时间
+}
+
+void ClanCopy::setStartTick(UInt32 tickCount)
+{
+    //  TODO: 设置游戏第一轮刷怪时间
+}
+
+void ClanCopy::process()
+{
+    // 定时器触发，每隔一秒更新一次状态
+    ++_copyProcessTime;
 }
 
 void ClanCopy::createEnemy(UInt32 round)
@@ -34,7 +52,7 @@ void ClanCopy::createEnemy(UInt32 round)
 
 void ClanCopy::roundMove(UInt32 round)
 {
-    // TODO: 副本移动
+    // 副本移动
     for (CopyMonster::iterator it = _copyMonster.begin(); it != _copyMonster.end(); ++it)
     {
         if ((it->second).hasMoved)
@@ -43,7 +61,7 @@ void ClanCopy::roundMove(UInt32 round)
 
         if (curSpotId == Copy_Spot::Home)
         {
-            // 敌人在主基地未打死不移动
+            // 敌人在主基地，不移动
             continue;
         }
         SpotList::iterator spotListIt = _spotList.find(curSpotId);
@@ -68,6 +86,7 @@ void ClanCopy::roundCombat(UInt8 round)
         SpotPlayer::iterator end = _spotPlayer.upper_bound(it->first);
         while (beg != end)
         {
+            // 寻找是否有可以匹配的玩家与怪物战斗
             if ((beg->second).formalBattleRound < round)
             {
                 // 找到可以匹配的玩家与怪物战斗
@@ -88,13 +107,15 @@ void ClanCopy::roundCombat(UInt8 round)
                 if (res)
                 {
                     // 玩家防守成功
-                    (beg->second).formalBattleRound = round;
-                    _copyMonster.erase(it);
+                    (beg->second).formalBattleRound = round; // 更新玩家战斗状态
+                    _copyMonster.erase(it); // 怪物消失
                 }
                 else
                 {
                     // 玩家防守失败，怪物可以走向下一个据点
-                    _spotPlayer.erase (beg);
+                    (beg->second).player->regenAll();    // 生命值回满
+                    _deadPlayer.insert (std::make_pair(beg->first, beg->second)); // 加入死亡名单
+                    _spotPlayer.erase (beg); // 从据点玩家中剔除
                 }
                 Stream st(REP::ATTACK_NPC);
                 st << static_cast<UInt8>(res) << static_cast<UInt8>(1) << static_cast<UInt8> (32) << static_cast<UInt8>(0) << static_cast<UInt8>(0);
@@ -106,17 +127,54 @@ void ClanCopy::roundCombat(UInt8 round)
             }
             ++beg;
         }
+
+        // 怪物轮空
+        if (beg == end && it->first == Home)
+        {
+            // 怪物在主基地轮空，攻击主基地
+            attackHome(it->second);
+            if (!_homeHp)
+            {
+                // 主基地被摧毁，副本挑战失败
+                lose();
+                return;
+            }
+        }
+    }
+}
+
+void ClanCopy::attackHome(const ClanCopyMonster& clanCopyMonster)
+{
+    // 怪物攻击主基地
+    if (_homeHp <= clanCopyMonster.npcValue)
+    {
+        // 主基地被摧毁
+        _homeHp = 0;
+        return;
+    }
+    else
+    {
+        _homeHp -= clanCopyMonster.npcValue;
     }
 }
 
 void ClanCopy::lose()
 {
     // TODO: 帮派副本挑战失败
+
+    for (SpotPlayer::iterator it = _spotPlayer.begin(); it != _spotPlayer.end(); ++ it)
+    {
+        (it->second).player->regenAll();
+    }
 }
 
 void ClanCopy::win()
 {
     // TODO: 帮派副本挑战成功
+    for (SpotPlayer::iterator it = _spotPlayer.begin(); it != _spotPlayer.end(); ++ it)
+    {
+        (it->second).player->regenAll();
+    }
 }
 
 
