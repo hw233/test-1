51ed47e845a901e6decb02cc68b657d5d202d3bc
nbits.y <nbits.y@gmail.com>
2012-09-24 19:05:12 +0800

    xxx

--- release.sh
+++ release.sh
@@ -11,20 +11,20 @@ file=$1
 if [ $# -ge 2 -a "$2" == "fb" ]; then
     make release_fb
     if [ $# -ge 3 -a "$3" == "s" ]; then
-        tar cvfz $R$file --exclude=sql/conf.sh bin/Release/Server.ASSS scripts/ sql/
+        tar cvfz $R$file -X exclude bin/Release/Server.ASSS scripts/ sql/
     else
         cp -rf sql sql.bak
         cp -rf scripts scripts.bak
         ./toft.sh
-        tar cvfz $R"FB."$file --exclude=sql/conf.sh bin/Release/Server.ASSS scripts/ sql/
+        tar cvfz $R"FB."$file -X exclude bin/Release/Server.ASSS scripts/ sql/
         rm -rf sql scripts
         mv sql.bak sql
         mv scripts.bak scripts
     fi
 elif [ $# -ge 2 -a "$2" == "vt" ]; then
     make release_vt
-    tar cvfz $R"VT."$file --exclude=sql/conf.sh  bin/Release/JsonProxy.ASSS bin/Release/Server.ASSS scripts/ sql/
+    tar cvfz $R"VT."$file -X exclude bin/Release/JsonProxy.ASSS bin/Release/Server.ASSS scripts/ sql/
 else
     make release
-    tar cvfz $R$file --exclude=sql/conf.sh  bin/Release/JsonProxy.ASSS bin/Release/Server.ASSS bin/Release/Server.ASSS.symbol scripts/ sql/
+    tar cvfz $R$file -X exclude bin/Release/JsonProxy.ASSS bin/Release/Server.ASSS bin/Release/Server.ASSS.symbol scripts/ sql/
 fi
