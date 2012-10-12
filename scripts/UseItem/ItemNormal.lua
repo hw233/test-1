9141c639b3fa9e02b96f62924b2e7af46e188fd8
qiweiyou <qiweiyou1982@163.com>
2012-10-12 14:08:19 +0800

    CDK10050-10051

--- scripts/UseItem/ItemNormal.lua
+++ scripts/UseItem/ItemNormal.lua
@@ -5578,6 +5578,43 @@ function ItemNormal_00010049(iid, num, bind, param)
     return num;
 end
 
+function ItemNormal_00010050(iid, num, bind, param)
+    local player = GetPlayer()
+    local package = player:GetPackage();
+
+    if package:GetRestPackageSize() < (5+(5*num*5)/99) then
+        player:sendMsgCode(2, 1011, 0);
+        return false
+    end
+
+    package:Add(1528, num*5, true, 0, 2);
+    package:Add(1325, num*5, true, 0, 2);
+    package:Add(515, num*5, true, 0, 2);
+    package:Add(503, num*5, true, 0, 2);
+    package:Add(9022, num*5, true, 0, 2);
+
+    package:DelItemSendMsg(iid, player);
+    return num;
+end
+
+function ItemNormal_00010051(iid, num, bind, param)
+    local player = GetPlayer()
+    local package = player:GetPackage();
+
+    if package:GetRestPackageSize() < (4+(4*num*8)/99) then
+        player:sendMsgCode(2, 1011, 0);
+        return false
+    end
+
+    package:Add(9076, num*8, true, 0, 2);
+    package:Add(515, num*8, true, 0, 2);
+    package:Add(440, num*8, true, 0, 2);
+    package:Add(516, num*8, true, 0, 2);
+
+    package:DelItemSendMsg(iid, player);
+    return num;
+end
+
 function ItemNormal_QixiLoveCard(iid, num, bind, param)
     local player = GetPlayer()
     local package = player:GetPackage();
@@ -7314,6 +7351,8 @@ local ItemNormal_Table = {
     [10047] = ItemNormal_00010047,
     [10048] = ItemNormal_00010048,
     [10049] = ItemNormal_00010049,
+    [10050] = ItemNormal_00010050,
+    [10051] = ItemNormal_00010051,
 };
 
 function ItemNormalOther_00000441(iid, num, bind, other)
