62db00e14e668b794dfc5ea3b813d49eb20da659
yij <yij@gmail.com>
2012-10-18 00:02:17 +0800

    增加手动开启天劫

--- sql/object.sh
+++ sql/object.sh
@@ -76,6 +76,7 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120831_1.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120911_1.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120915_1.sql
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20120928_1.sql
     mysql -h$H -u$U -p$P -P$PT $DBO < updates/cleartotalrecharge.sql # must be once for delete recharge total
     mysql -h$H -u$U -p$P -P$PT $DBO < updates/clearconsume.sql # must be once for delete total consume
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/clear3366totalrecharge.sql # must be once for delete recharge total
