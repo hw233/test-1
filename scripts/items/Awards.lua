72879ea295b3fe2459fe8860eeb4e368f4dac71a
yij <yij@gmail.com>
2012-11-01 00:39:30 +0800

    黄钻活动天数+1

--- scripts/items/Awards.lua
+++ scripts/items/Awards.lua
@@ -225,7 +225,7 @@ function RunBlueDiamondAward(player, opt)
     local date_9190_0 = { ['year'] = 2012, ['month'] = 10, ['day'] = 26, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 }
     local date_9190_1 = { ['year'] = 2012, ['month'] = 11, ['day'] = 2, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 }
     local date_9191_0 = { ['year'] = 2012, ['month'] = 10, ['day'] = 27, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 }
-    local date_9191_1 = { ['year'] = 2012, ['month'] = 11, ['day'] = 3, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 }
+    local date_9191_1 = { ['year'] = 2012, ['month'] = 11, ['day'] = 4, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 }
     local date_0 = os.time(date_9190_0);
     local date_1 = os.time(date_9190_1);
 
