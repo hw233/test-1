e30aac59088264a228a4da236a5111ccb6c1fb3b
james <zhgc1983@163.com>
2012-11-29 02:05:46 +0800

    merge dev

--- Core/GObject/GObjectManager.cpp
+++ Core/GObject/GObjectManager.cpp
@@ -5039,60 +5039,6 @@ namespace GObject
 
     }
 
-    bool GObjectManager::loadArenaExtraBoard()
-    {
-		std::unique_ptr<DB::DBExecutor> execu(DB::gObjectDBConnectionMgr->GetExecutor());
-		if (execu.get() == NULL || !execu->isConnected()) return false;
-		LoadingCounter lc("Loading arena_extra_board");
-        DBArenaExtraBoard extraboard;
-        if(execu->Prepare("SELECT `week`, `name1`, `name2`, `name3`, `name4`, `name5`, `heroId1`, `heroId2`, `heroId3`, `heroId4`, `heroId5`, `sufferTotal`, `sufferCnt1`, `sufferCnt2`, `sufferCnt3`, `sufferCnt4`, `sufferCnt5`, `lasttime1`, `lasttime2`, `lasttime3`, `lasttime4`, `lasttime5` FROM `arena_extra_board` ORDER BY `week`", extraboard) != DB::DB_OK)
-			return false;
-		lc.reset(1000);
-		while(execu->Next() == DB::DB_OK)
-        {
-			lc.advance();
-            UInt8 week = extraboard.week;
-			if(week == ARENA_WEEK_START || week == ARENA_WEEK_END)
-            {
-                UInt8 type = week - ARENA_WEEK_START;
-                GObject::World::stArenaOld[type].week = extraboard.week;
-                GObject::World::stArenaOld[type].sufferTotal = extraboard.sufferTotal;
-                for(UInt8 i = 0; i < 5; i++)
-                {
-                    GObject::World::stArenaOld[type].name[i] = extraboard.name[i];
-                    GObject::World::stArenaOld[type].heroId[i] = extraboard.heroId[i];
-                    GObject::World::stArenaOld[type].sufferCnt[i] = extraboard.sufferCnt[i];
-                    GObject::World::stArenaOld[type].lasttime[i] = extraboard.lasttime[i];
-                }
-
-                ValueSort cur;
-                ValueSortType resultRank;
-                for(UInt8 i = 0; i < 5; i++)
-                {
-                    cur.sufferCnt = World::stArenaOld[type].sufferCnt[i];
-                    cur.lastTime = World::stArenaOld[type].lasttime[i];
-                    cur.name = World::stArenaOld[type].name[i];
-                    //std::cout << (UInt32)i << "::: " << cur.name << std::endl;
-                    resultRank.insert(cur);
-                }
-                for(UInt8 i = 0; i < 5; i++)
-                {
-                    UInt8 j = 0;
-                    for(ValueSortType::iterator iter = resultRank.begin(), e = resultRank.end(); iter != e && j < 5; ++iter, ++j)
-                    {
-                        //std::cout << (UInt32)j << ": " << (*iter).name << std::endl;
-                        if((*iter).name == World::stArenaOld[type].name[i])
-                            break;
-                    }
-                    //std::cout << "result: " << (UInt32)(j+1) << std::endl;
-                    World::stArenaOld[type].rank[i] = j + 1;
-                }
-            }
-        }
-        lc.finalize();
-        return true;
-    }
-
     bool GObjectManager::loadJobHunter()
     {
         // 读取寻墨有关数据
