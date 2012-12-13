ff10fa44371d979268865c6c831ed2fe599795ae
james <zhgc1983@163.com>
2012-12-13 18:33:12 +0800

    battle report

--- Core/Battle/BattleReport.h
+++ Core/Battle/BattleReport.h
@@ -15,9 +15,27 @@ public:
 	std::vector<UInt8>* getTitleBattle();
 private:
 	void loadReport(UInt32, std::vector<UInt8>&);
+    void travelRptLife();
 private:
 	FastMutex _mutex;
 	std::map<UInt32, std::vector<UInt8> > _reports;
+
+    struct RptLife
+    {
+        UInt32 _rptId;
+        UInt32 _loadTime;
+        bool operator< (const RptLife& rptLife) const
+        {
+            if(_loadTime == rptLife._loadTime)
+                return _rptId < rptLife._rptId;
+
+            return _loadTime < rptLife._loadTime;
+        }
+    };
+    typedef std::set<RptLife> RptLifeSet;
+    typedef RptLifeSet::iterator RptLifeSetIt;
+
+    RptLifeSet _rptLife;
 };
 
 extern BattleReport battleReport;
