b6214f5893402b8e0f68cbf1ecb126ecaa61f90b
yij <yij@gmail.com>
2012-12-21 21:12:53 +0800

    增加红钻抽奖

--- scripts/items/Awards.lua
+++ scripts/items/Awards.lua
@@ -402,11 +402,15 @@ function RunBlueDiamondAward(player, opt)
     local date_1 = os.time(date_9190_1);
 
     local chance = {1852, 2593, 3333, 4444, 5926, 6667, 8148, 10000}
+    local chance_1 = {1428, 1428*2, 1428*3, 1428*4, 1428*5, 1428*6, 10000, 10000}
     local item_9190 = {{515,3},{507,2},{509,2},{503,10},{1325,4},{47,3},{134,4},{5026,1}}
     local item_9191 = {{515,3},{507,3},{509,3},{503,10},{1325,4},{47,3},{134,4},{5026,1}}
     local item_9217 = {{515,3},{507,2},{509,2},{503,10},{1325,4},{47,3},{134,4},{5026,1}}
+    local item_9284 = {{515,3},{507,2},{509,2},{503,10},{1325,4},{47,3},{134,4},{'ipad',1}}
+
     local items = item_9190;
     local itemId = 9190;
+    local ch = chance;
 
     if opt == 2 then
         itemId = 9191;
@@ -418,6 +422,10 @@ function RunBlueDiamondAward(player, opt)
         items = item_9217;
         date_0 = os.time(date_9217_0);
         date_1 = os.time(date_9217_1);
+    elseif opt == 4 then
+        itemId = 9284;
+        items = item_9284;
+        ch = chance_1;
     end
 --    now = os.time()
 --    if now < date_0 or now >= date_1 then
@@ -433,8 +441,8 @@ function RunBlueDiamondAward(player, opt)
 
     local j = 0; 
     local g = math.random(1, 10000)
-    for i = 1, #chance do
-        if g <= chance[i] then
+    for i = 1, #ch do
+        if g <= ch[i] then
             package:Add(items[i][1], items[i][2], true, true, 31);
             j = i;
             break
