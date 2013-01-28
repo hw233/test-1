bc05654bea47a7b2c233246f648ce8b133ba416a
james <zhgc1983@163.com>
2013-01-28 14:03:46 +0800

    sql

--- sql/updates/Object_20130116_1.sql
+++ sql/updates/Object_20130116_1.sql
@@ -1,3 +1,1 @@
-alter TABLE `discount` DROP COLUMN discount exValue int(10) unsigned NOT NULL;
-alter TABLE `discount` ADD COLUMN `exType` tinyint(3) unsigned NOT NULL after `itemid`;
-alter TABLE `discount` ADD COLUMN `exValue` int(10) unsigned NOT NULL after `exType`;
+alter TABLE `discount` DROP COLUMN discount;
