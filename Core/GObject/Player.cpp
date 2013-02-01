62473eb57d9f8676100435449637dbe586782120
zhgc <zhgc1983@gmail.com>
2013-02-01 10:03:39 +0800

    equipto lingbao

--- Core/GObject/Player.cpp
+++ Core/GObject/Player.cpp
@@ -2789,6 +2789,8 @@ namespace GObject
             for(UInt8 t = 0; t < 3; ++ t)
 				m_Package->EquipTo(0, fgt, t+0x0a, equip, true);
             m_Package->EquipTo(0, fgt, 0x1f, equip, true);
+            for(UInt8 t = 0; t < 3; ++ t)
+				m_Package->EquipTo(0, fgt, t+0x60, equip, true);
 
 			_fighters.erase(it);
 			DB2().PushUpdateData("DELETE FROM `fighter` WHERE `id` = %u AND `playerId` = %"I64_FMT"u", id, getId());
