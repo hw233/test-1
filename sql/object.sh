d2971985e8c50eb2271594bd8b13f2c545e159f5
suntao <suntao@gmail.com>
2013-03-25 16:59:46 +0800

    清除大闹龙宫var

--- sql/object.sh
+++ sql/object.sh
@@ -154,7 +154,7 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130304_1.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130225_1.sql          #帮派QQ群
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130315_1.sql # must be once 有删除操作
-    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130325_1.sql # must be once 有删除操作
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130326_1.sql # must be once 有删除操作
 
     #. clearshiyamen.sh
 }
