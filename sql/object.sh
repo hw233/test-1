3a5a9e515eda50160010ff214c6f697491608857
suntao <suntao@gmail.com>
2013-04-03 20:53:42 +0800

    注释sql脚本

--- sql/object.sh
+++ sql/object.sh
@@ -157,7 +157,7 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130319_1.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130326_1.sql # must be once 有删除操作
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/clearbluediamond.sql  #开蓝黄钻活动需要打开 
-    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130403_1.sql # must be once 有删除操作
+    #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130403_1.sql # must be once 有删除操作
 
     #. clearshiyamen.sh
 }
