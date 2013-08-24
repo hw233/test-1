0da42f0f615b0b8269d2d2c3966eeb3cc4b04e35
nbits.y <nbits.y@gmail.com>
2013-08-24 12:01:45 +0800

    xxx

--- sql/object.sh
+++ sql/object.sh
@@ -188,12 +188,12 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_alter_friendkey.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_create_collect_card.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_clean_mofang.sql
-    #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_alter_jiguan.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/clearxyx.sql #清除幸运星购买次数，开启幸运星活动时开启
     #mysql -h$H -u$U -p$P -P$PT $DBO < player_tuzhi.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < player_jiguanshu.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < player_jiguanyu.sql
-    #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_create_pray_relation.sql
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_alter_jiguan.sql
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_create_pray_relation.sql
     #. clearshiyamen.sh
 }
 
--- sql/updates/Object_alter_jiguan.sql
+++ sql/updates/Object_alter_jiguan.sql
@@ -1,5 +1,3 @@
 ALTER TABLE `player_jiguanshu` modify column `playerId` bigint(20) unsigned NOT NULL DEFAULT '0';
-
 ALTER TABLE `player_jiguanyu` modify column `playerId` bigint(20) unsigned NOT NULL DEFAULT '0';
-
 ALTER TABLE `player_tuzhi` modify column `playerId` bigint(20) unsigned NOT NULL DEFAULT '0';
