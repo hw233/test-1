c6b643a0db47d2d0b4325bfa1a5e9ed2e9a95376
suntao <suntao@gmail.com>
2013-08-29 16:14:16 +0800

    Q点直购

--- sql/object.sh
+++ sql/object.sh
@@ -187,13 +187,13 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_alter_friendkey.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_create_collect_card.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_clean_mofang.sql
-    #mysql -h$H -u$U -p$P -P$PT $DBO < updates/clearxyx.sql #清除幸运星购买次数，开启幸运星活动时开启
     #mysql -h$H -u$U -p$P -P$PT $DBO < player_tuzhi.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < player_jiguanshu.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < player_jiguanyu.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_create_pray_relation.sql
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_correct_pray_relation.sql
     mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20130429_1.sql   #清除节日套装(var294)
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/clearxyx.sql #清除幸运星购买次数，开启幸运星活动时开启
     #. clearshiyamen.sh
 }
 
