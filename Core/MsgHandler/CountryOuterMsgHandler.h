226b76d4b9545c9e016cf40492be4bac3ab2c807
zhgc <zhgc1983@gmail.com>
2013-09-12 20:33:16 +0800

    dungeon

--- Core/MsgHandler/CountryOuterMsgHandler.h
+++ Core/MsgHandler/CountryOuterMsgHandler.h
@@ -2487,7 +2487,7 @@ void OnDungeonOpReq( GameMsgHdr& hdr, DungeonOpReq& dor )
 
 void OnDungeonInfoReq( GameMsgHdr& hdr, DungeonInfoReq& dir )
 {
-	if(dir.difficulty >= 2)
+	if(dir.difficulty >= Max_Difficulty)
         return;
     
     MSG_QUERY_PLAYER(pl);
