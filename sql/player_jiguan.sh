4a6e5832c26da3d89469efebcb01b3a904761cc4
nbits.y <nbits.y@gmail.com>
2013-09-15 18:29:32 +0800

    xxx

--- sql/player_jiguan.sh
+++ sql/player_jiguan.sh
@@ -1,39 +1,30 @@
-files=`ls /data/run/run*/log/DB/INFO20130821.gz`
-for f in $files
-do
-    echo $f
-    ff=`dirname $f`
-    ff=`dirname $ff`
-    ff=`dirname $ff`
-    n=`basename $ff`
-    zcat $f | grep player_tuzhi|awk -F[][] '{printf "%s;\n", $6}' > $ff/sql/player_tuzhi.sql
-    zcat $f | grep player_jiguanshu|awk -F[][] '{printf "%s;\n", $6}' > $ff/sql/player_jiguanshu.sql
-    zcat $f | grep player_jiguanyu|awk -F[][] '{printf "%s;\n", $6}' > $ff/sql/player_jiguanyu.sql
-done
+f=../log/DB/INFO20130821.gz
+echo $f
+ff=`dirname $f`
+ff=`dirname $ff`
+ff=`dirname $ff`
+n=`basename $ff`
+zcat $f | grep player_tuzhi|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] > $ff/sql/player_tuzhi.sql
+zcat $f | grep player_jiguanshu|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] > $ff/sql/player_jiguanshu.sql
+zcat $f | grep player_jiguanyu|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] > $ff/sql/player_jiguanyu.sql
 
-files=`ls /data/run/run*/log/DB/INFO20130822.gz`
-for f in $files
-do
-    echo $f
-    ff=`dirname $f`
-    ff=`dirname $ff`
-    ff=`dirname $ff`
-    n=`basename $ff`
-    zcat $f | grep player_tuzhi|awk -F[][] '{printf "%s;\n", $6}' >> $ff/sql/player_tuzhi.sql
-    zcat $f | grep player_jiguanshu|awk -F[][] '{printf "%s;\n", $6}' >> $ff/sql/player_jiguanshu.sql
-    zcat $f | grep player_jiguanyu|awk -F[][] '{printf "%s;\n", $6}' >> $ff/sql/player_jiguanyu.sql
-done
+f=../log/DB/INFO20130822.gz
+echo $f
+ff=`dirname $f`
+ff=`dirname $ff`
+ff=`dirname $ff`
+n=`basename $ff`
+zcat $f | grep player_tuzhi|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] >> $ff/sql/player_tuzhi.sql
+zcat $f | grep player_jiguanshu|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] >> $ff/sql/player_jiguanshu.sql
+zcat $f | grep player_jiguanyu|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] >> $ff/sql/player_jiguanyu.sql
 
-files=`ls /data/run/run*/log/DB/INFO20130823`
-for f in $files
-do
-    echo $f
-    ff=`dirname $f`
-    ff=`dirname $ff`
-    ff=`dirname $ff`
-    n=`basename $ff`
-    cat $f | grep player_tuzhi|awk -F[][] '{printf "%s;\n", $6}' >> $ff/sql/player_tuzhi.sql
-    cat $f | grep player_jiguanshu|awk -F[][] '{printf "%s;\n", $6}' >> $ff/sql/player_jiguanshu.sql
-    cat $f | grep player_jiguanyu|awk -F[][] '{printf "%s;\n", $6}' >> $ff/sql/player_jiguanyu.sql
-done
+f=../log/DB/INFO20130823
+echo $f
+ff=`dirname $f`
+ff=`dirname $ff`
+ff=`dirname $ff`
+n=`basename $ff`
+cat $f | grep player_tuzhi|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] >> $ff/sql/player_tuzhi.sql
+cat $f | grep player_jiguanshu|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] >> $ff/sql/player_jiguanshu.sql
+cat $f | grep player_jiguanyu|awk -F[][] '{printf "%s;\n", $6}' | grep ^[RD] >> $ff/sql/player_jiguanyu.sql
 
