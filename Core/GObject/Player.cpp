7cf9f8d99405505df3b25e0addbac44cfa57e91f
lib <498714364@qq.com>
2013-10-07 11:11:10 +0800

    XX

--- Core/GObject/Player.cpp
+++ Core/GObject/Player.cpp
@@ -13178,7 +13178,7 @@ namespace GObject
         UInt32 off =(TimeUtil::SharpDay(0, now)-TimeUtil::SharpDay(0, begin))/86400 +1;
         if(now < begin || now >end )
             return ;
-        if(off!= 1 && !(LuckyMeet&(1<<(off-1))))
+        if(off!= 1 && !(LuckyMeet&(1<<(off-1)))&&!(LuckyMeet&(1<<(off-2))))
           SetVar(VAR_LUCKYMEET_AWARD , 0); 
         LuckyMeet |= 1 << (off - 1);
         SetVar(VAR_LUCKYMEET, LuckyMeet);
@@ -13208,7 +13208,7 @@ namespace GObject
         UInt32 off =(TimeUtil::SharpDay(0, now)-TimeUtil::SharpDay(0, begin))/86400 +1;
         if(now < begin || now >end )
             return ;
-        if(off!= 1 && !(SummerMeetLogin&(1<<(off-2))))
+        if(off!= 1 && !(SummerMeetLogin&(1<<(off-1)))&&!(SummerMeetLogin&(1<<(off-2))))
             SetVar(VAR_SUMMER_MEET_LOGIN_AWARD , 0); 
         SummerMeetLogin |= 1 << (off - 1);
         SetVar(VAR_SUMMER_MEET_LOGIN, SummerMeetLogin);
