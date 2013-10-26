f6c32b9d0b2abf020f821fe1ff7bb5fcb2ae7308
ljr <ljr@gmail.com>
2013-10-27 02:05:08 +0800

    修改更新

---
+++ sql/updates/Object_20131018_2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE lingbaosmelt ADD COLUMN counts tinyint(3) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE lingbaosmelt ADD COLUMN purpleAdjVal tinyint(3) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE lingbaosmelt ADD COLUMN orangeAdjVal tinyint(3) unsigned NOT NULL DEFAULT '0';
