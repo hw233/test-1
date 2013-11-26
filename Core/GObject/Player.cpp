1322390675f64ae3711a9607c43d63aa649dd524
qiwy <qiwy@kingnet.com>
2013-11-26 20:58:43 +0800

    天劫

--- Core/GObject/Player.cpp
+++ Core/GObject/Player.cpp
@@ -16960,7 +16960,10 @@ namespace GObject
                         SetVar(VAR_TJ_AUTO_FRONTMAP_END_TIME, TimeUtil::Now());
                     UInt32 now = TimeUtil::Now();
                     if(TimeUtil::SharpDay(0, now) != TimeUtil::SharpDay(0, GetVar(VAR_TJ_AUTO_FRONTMAP_END_TIME)))
+                    {
                         SetVar(VAR_TJ_TASK3_COPYID, 0);
+                        SetVar(VAR_TJ_AUTO_FRONTMAP_END_TIME, now);
+                    }
 
                     if (GetVar(VAR_TJ_TASK3_COPYID) >= 51)
                     {
@@ -17130,6 +17133,7 @@ namespace GObject
             if(TimeUtil::SharpDay(0, now) != TimeUtil::SharpDay(0, GetVar(VAR_TJ_AUTO_FRONTMAP_END_TIME)))
             {
                 SetVar(VAR_TJ_TASK3_COPYID, 0);
+                SetVar(VAR_TJ_AUTO_FRONTMAP_END_TIME, now);
                 finish = 0;
                 copyid = 0;
             }
@@ -17380,7 +17384,10 @@ void EventTlzAuto::Process(UInt32 leftCount)
         m_Player->delFlag(Player::AutoTlz);
         UInt32 now = TimeUtil::Now();
         if(TimeUtil::SharpDay(0, now) != TimeUtil::SharpDay(0, m_Player->GetVar(VAR_TJ_AUTO_FRONTMAP_END_TIME)))
+        {
             m_Player->SetVar(VAR_TJ_TASK3_COPYID, 0);
+            m_Player->SetVar(VAR_TJ_AUTO_FRONTMAP_END_TIME, TimeUtil::Now());
+        }
     }
 }
 
