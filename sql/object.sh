4bf1d5fc325a420ab7caf0ede30760a94cb02512
lib <498714364@qq.com>
2014-01-24 17:46:23 +0800

    清空捉迷藏

--- sql/object.sh
+++ sql/object.sh
@@ -248,6 +248,7 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_create_sevensoul.sql  #创建七魄
     mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_SetGVar79_Marry.sql  #设置夫妻对数
     mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_AriBook_1121.sql     #天书奇缘清空
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_clearOldMan.sql     #天书奇缘清空
 }
 
 #sh player_jiguan.sh
