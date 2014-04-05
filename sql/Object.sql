7277597ec3a05b44d02c22a2650714b83ab6c7c9
qiwy <qiwy@kingnet.com>
2014-04-05 20:05:29 +0800

    sql

--- sql/Object.sql
+++ sql/Object.sql
@@ -2457,3 +2457,12 @@ CREATE TABLE `zhenyuanAttr` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `skill_grade`;
+CREATE TABLE `skill_grade` (
+    `playerId` bigint(20) unsigned NOT NULL,
+    `fighterId` int(10) unsigned NOT NULL,
+    `skillId` smallint(5) unsigned NOT NULL,
+    `level` tinyint(3) unsigned NOT NULL DEFAULT '0',
+    PRIMARY KEY(`playerId`, `fighterId`, `skillId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
