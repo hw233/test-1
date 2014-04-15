f62fba0f3047fa5ffaa3f7994eeba7ed037f04e4
ljr <ljr@gmail.com>
2014-04-16 05:10:58 +0800

    抗击天魔gvar

--- Core/GObject/GVar.h
+++ Core/GObject/GVar.h
@@ -142,14 +142,14 @@ namespace GObject
         GVAR_DUOBAO_ENDTIME = 86,       // 夺宝每轮结束时间
         GVAR_CLAN_DUOBAO_BEGIN = 87,    // 夺宝每天开始时间
         GVAR_CLAN_DUOBAO_END = 88,      // 夺宝每天结束时间
-        GVAR_KANGJITIANMO_BEGIN = 89,   // 抗击天魔开始时间
-        GVAR_KANGJITIANMO_END = 90,     // 抗击天魔结束时间
-        GVAR_MAX_TEAM_NUM = 91,         // 抗击天魔最大队伍数量
-        
-        GVAR_TYSS_BEGIN= 89,      // 天元神兽开启时间
-        GVAR_TYSS_END= 90,      // 天元神兽结束时间
-        
-        GVAR_REPAIRTYSSBUG = 91,//天元神兽bug修复
+                
+        GVAR_TYSS_BEGIN= 89,        // 天元神兽开启时间
+        GVAR_TYSS_END= 90,          // 天元神兽结束时间
+        GVAR_REPAIRTYSSBUG = 91,    //天元神兽bug修复
+
+        GVAR_KANGJITIANMO_BEGIN = 92,   // 抗击天魔开始时间
+        GVAR_KANGJITIANMO_END = 93,     // 抗击天魔结束时间
+        GVAR_MAX_TEAM_NUM = 94,         // 抗击天魔最大队伍数量
 
         GVAR_MAX,
     };
