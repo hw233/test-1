e9be2c6435e39d299ec381d6f06df91c277ee4ad
lib <498714364@qq.com>
2014-04-19 02:45:16 +0800

    充值天池

--- scripts/World/Activity.lua
+++ scripts/World/Activity.lua
@@ -807,7 +807,7 @@ function onActivityCheck(tm)
       elseif tm >= actTime2013_04_09 and tm < actTime2013_04_09+5*86400 then
           setRechargeActive(true, 16)
           setNeedRechargeRank(true)
-      elseif tm >= actTime2013_04_14 and tm < actTime2013_04_14+356*86400 then
+      elseif tm >= actTime2013_04_14 and tm < actTime2013_04_14+370*86400 then
           -- RECHARGE
           setRechargeActive(true, 16)
           setNeedRechargeRank(true)
@@ -1161,7 +1161,7 @@ function onActivityCheck(tm)
   end
 
   -- ACC
-  if tm >= actTime2014_01_25 and tm < (actTime2014_01_25 + 71*86400) then
+  if tm >= actTime2014_01_25 and tm < (actTime2014_01_25 + 85*86400) then
       setAccRecharge(true)
   else
       setAccRecharge(false)
