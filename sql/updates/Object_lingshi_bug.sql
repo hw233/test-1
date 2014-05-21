1f635b51885311e45bcf0ffd54dd797312ef717d
qiwy <qiwy@kingnet.com>
2014-05-21 17:00:26 +0800

    灵侍bug

---
+++ sql/updates/Object_lingshi_bug.sql
@@ -0,0 +1,10 @@
+ update lingshiAttr set exp = 324 where level > 0 and level < 9;
+ update lingshiAttr set exp = 2128 where level > 9 and level < 19;
+ update lingshiAttr set exp = 6612 where level > 19 and level < 29;
+ update lingshiAttr set exp = 14976 where level > 29 and level < 39;
+ update lingshiAttr set exp = 28420 where level > 39 and level < 49;
+ update lingshiAttr set exp = 48144 where level > 49 and level < 59;
+ update lingshiAttr set exp = 75348 where level > 59 and level < 69;
+ update lingshiAttr set exp = 111232 where level > 69 and level < 79;
+ update lingshiAttr set exp = 155998 where level > 79 and level < 89;
+ update lingshiAttr,fighter set lingshiAttr.level = lingshiAttr.level - (lingshiAttr.level % 10) + 9 where fighter.level >= 85 and (lingshiAttr.level % 10) != 9 and fighter.lingshi like concat("%",lingshiAttr.id,"%");
