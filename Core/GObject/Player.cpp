afb82f026f55a57026c008548abf882030b562db
qimingjian <n.comoon@gmail.com>
2014-05-22 11:03:09 +0800

    140级副本

--- Core/GObject/Player.cpp
+++ Core/GObject/Player.cpp
@@ -20628,7 +20628,7 @@ void Player::sendCopyFrontAllAward()
 
 UInt8 Player::getCopyId()
 {
-    static UInt16 spots[] = {776, 2067, 5906, 8198, 12818, 10512, 0x1411, 0x2707, 0x290a, 4871};
+    static UInt16 spots[] = {776, 2067, 5906, 8198, 12818, 10512, 0x1411, 0x2707, 0x290a, 4871, 4628};
 
     UInt16 currentSpot = PLAYER_DATA(this, location);
     for(UInt8 i = 0; i < sizeof(spots)/sizeof(spots[0]); i++)
