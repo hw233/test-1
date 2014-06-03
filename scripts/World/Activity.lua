defff2c1a44c6f4a69efee6d0446eac283793903
qimingjian <n.comoon@gmail.com>
2014-06-03 12:42:29 +0800

    六一套装,六一领奖，日月合壁，招财进宝

--- scripts/World/Activity.lua
+++ scripts/World/Activity.lua
@@ -1411,7 +1411,7 @@ function initActTime(y, m, d)
   local  SerStartTm231 = { ['year'] = 2012, ['month'] = 12, ['day'] = 30, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   local  SerStartTm231_1 = { ['year'] = 2013, ['month'] = 1, ['day'] = 6, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   --节日套装-五道杠
-  local  SerStartTm232 = { ['year'] = 2014, ['month'] = 4, ['day'] = 27, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
+  local  SerStartTm232 = { ['year'] = 2014, ['month'] = 5, ['day'] = 28, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   -- 锦云兜商城
   local  SerStartTm233 = { ['year'] = 2013, ['month'] = 1, ['day'] = 11, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   --英雄岛限时“自动战斗”活动
@@ -1439,7 +1439,7 @@ function initActTime(y, m, d)
   local  SerStartTm2013_03_15 = { ['year'] = 2013, ['month'] = 3, ['day'] = 15, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
 
   -- 迎新纳福 红包入袋
-  local  SerStartTm237 = { ['year'] = 2014, ['month'] = 5, ['day'] = 1, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
+  local  SerStartTm237 = { ['year'] = 2014, ['month'] = 6, ['day'] = 2, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   -- 蓝钻新年回馈活动
   local  SerStartTm238 = { ['year'] = 2013, ['month'] = 2, ['day'] = 4, ['hour'] = 0, ['min'] = 0, ['sec'] = 0 };
   -- 空间朋友连续登录奖励
@@ -1896,7 +1896,7 @@ function initActTime(y, m, d)
   actTime231_1= os.time(SerStartTm231_1);
 
   actTime232_0= os.time(SerStartTm232);
-  actTime232_1= os.time(SerStartTm232) + 8 * 86400;
+  actTime232_1= os.time(SerStartTm232) + 9 * 86400;
 
   actTime233_0= os.time(SerStartTm233);
   actTime233_1= os.time(SerStartTm233) + 7 * 86400;
