5ce552440bab21756c22998ea28f4cf2c90164d6
qiwy <qiwy@kingnet.com>
2014-06-20 14:07:37 +0800

    Data.sql

--- sql/Data.sql
+++ sql/Data.sql
@@ -1194,7 +1194,7 @@ CREATE TABLE `lingshi_level` (
     `level` tinyint(3) unsigned NOT NULL DEFAULT 0,
     `consume` int(10) unsigned NOT NULL DEFAULT 0,
     `exp` bigint(20) unsigned NOT NULL DEFAULT 0,
-    `isUp` tinyint(3) unsigned NOT NULL DEFAULT 0,
+    `isBreak` tinyint(3) unsigned NOT NULL DEFAULT 0,
     `useItem` int(10) unsigned NOT NULL DEFAULT 0,
     `useGold` int(10) unsigned NOT NULL DEFAULT 0,
     `attack` float(10,2) unsigned NOT NULL DEFAULT '0.00',
