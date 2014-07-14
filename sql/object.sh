31486587c0398e61902d07968989a91579918587
qiwy <qiwy@kingnet.com>
2014-07-15 01:58:17 +0800

    修复天元神兽阳光普照buff

--- sql/object.sh
+++ sql/object.sh
@@ -328,6 +328,7 @@ function update_object()
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_clearcarditem.sql     #清除称号卡物品
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_alter_jiguanyu_1.sql   #修改player_jiguanyu表主索引
     mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_AriBook_1121.sql     #天书奇缘清空
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_tyss_buff6b.sql     #天元神兽阳光普照buff
 }
 
 #sh player_jiguan.sh
