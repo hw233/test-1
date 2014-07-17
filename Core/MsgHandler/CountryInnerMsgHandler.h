882afb1910a9280789f768284a344c3f2a1736db
qiwy <qiwy@kingnet.com>
2014-07-17 20:28:02 +0800

    Q点直钩

--- Core/MsgHandler/CountryInnerMsgHandler.h
+++ Core/MsgHandler/CountryInnerMsgHandler.h
@@ -1116,7 +1116,7 @@ void OnDirectPurchase( GameMsgHdr& hdr, const void * data )
     }
 
     pkg->AddItem(pur->id, pur->num, true, false, FromDirectPurchase);
-    if(pur->id == 9338 || pur->id == 9414 || pur->id == 9141)
+    if(pur->id == 9388 || pur->id == 9414 || pur->id == 9141)
         player->AddVar(VAR_DIRECTPURCNT, 1);
     else
         player->AddVar(VAR_DIRECTPURCNT2, 1);
