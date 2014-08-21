7528144022ee4ae11c28180c0f57fa1cc528d2c2
dtc <dtcisbug@gmail.com>
2014-08-22 01:06:06 +0800

    xx

--- Core/GObject/DarkDargon.cpp
+++ Core/GObject/DarkDargon.cpp
@@ -10,7 +10,7 @@
 #include <algorithm>
 #include "WBossMgr.h"
 
-#define _TEST_VECSION
+//#define _TEST_VECSION
 
 const UInt16 MONSTER = 13200;
 const UInt16 MAPMONSTER = 13201;
@@ -1211,7 +1211,10 @@ void DarkDargon::AddDDScore(DDPlayer* ddpl,Int32 score)
         s_pss.insert(ps);
         ddpl->score = ps.score; 
         if(score > 0)
+        {
             SYSMSG_SENDV(5194, ddpl->player, score);
+            SYSMSG_SENDV(5199, ddpl->player, score);
+        }
     }
     /*else
     {
