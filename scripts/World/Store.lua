c45eb0c752802481422650259b595013cfdb601f
dtc <dtcisbug@gmail.com>
2014-09-02 10:40:59 +0800

    庆典卡牌

--- scripts/World/Store.lua
+++ scripts/World/Store.lua
@@ -132,6 +132,9 @@ function loadStore()
     if getSummerCardActivity(300) then --清凉令
         store:add(2 ,17011,20)
     end
+    if getCelebrateCardActivity(300) then --庆典令
+        store:add(2 ,17026,20)
+    end
     if get61CardActivity(300) then --童心令
         store:add(2 ,9895,20)
     end
