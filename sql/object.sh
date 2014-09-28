af3f5613651707823c7a1e2787b5277a92d45189
qimingjian <n.comoon@gmail.com>
2014-09-28 19:53:48 +0800

    x

--- sql/object.sh
+++ sql/object.sh
@@ -167,7 +167,8 @@ function update_object()
     #增加的表中如果有fighterId，记得在useChangeSexCard函数中添加修改主将ID的函数
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_create_questiopaper.sql   #调查问卷
     #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_20140915_clearCopyGold.sql   #清除当日付费次数
-    #mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_AriBook_1121.sql     #天书奇缘清空
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_AriBook_1121.sql     #天书奇缘清空
+    mysql -h$H -u$U -p$P -P$PT $DBO < updates/Object_clearCoolSummer.sql     #清除酷爽一夏
 }
 
 #sh player_jiguan.sh
