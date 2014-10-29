1bc29dea457f3432a75439d2cd26e6f178fdec71
qiwy <qiwy@kingnet.com>
2014-10-29 18:24:40 +0800

    扩展属性

--- scripts/formula/personal.lua
+++ scripts/formula/personal.lua
@@ -535,10 +535,6 @@ function calcPierce( fgt, defgt )
 end
 
 function calcDamage( atk, def, atklvl, toughFactor, dmgreduce, attackpierce )
-    def = def - attackpierce
-    if def < 0 then
-        def = 0
-    end
     local dmgP = (1 - def/(def + deflvl_factor*atklvl + deflvl_addon_factor) * toughFactor - dmgreduce/100 + attackpierce / 100)
     if dmgP < 0.20 then
         dmgP = 0.20
