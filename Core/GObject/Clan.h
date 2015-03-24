c7021aa9477ab5b23a0645a3bc33cd56167fd6e9
libo <498714364@qq.com>
2015-03-23 19:12:59 -0700

    X

--- Core/GObject/Clan.h
+++ Core/GObject/Clan.h
@@ -2,8 +2,10 @@
 #ifndef CLAN_H_
 #define CLAN_H_
 #include "Config.h"
+#include "Player.h"
 
 #define CLAN_MAX 60
+
 namespace GObject
 {
     class Player;
@@ -11,11 +13,22 @@ namespace GObject
     class Clan
     {
         public:
+            Clan(Player* creater, std::string name):_name(name),_creater(creater),_personMax(30){}
             void LoadPlayer(Player* pl);
             ChatHold * GetChatHold();
+
+            std::string GetName(){ return _name;}
+
         private:
             std::vector<Player* > _players;
             ChatHold * chatHold;
+
+            std::string _name;
+            std::string _announcement;
+            UInt8 _personMax;
+
+            Player* _creater;
+            Player* _leader;
     };
 }
 #endif // CLAN_H_
