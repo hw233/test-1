c15f06001be936df0f6a89290e5e747dc0a28498
libo <498714364@qq.com>
2015-07-19 16:02:55 +0800

    x

--- Core/Battle/BattleFighter.cpp
+++ Core/Battle/BattleFighter.cpp
@@ -45,13 +45,13 @@ namespace Battle
         //属性
         if( f->GetChildTypeId())
         {
+            UInt16 fighterId = f->GetChildTypeId();
             for(UInt8 i = 0; i < MYFIGHTERMAX ; ++i)
             {
-                UInt16 fighterId = f->GetChildTypeId();
-                GObject::Fighter * f = GObject::globalFighters[fighterId];
-                if(!f)
+                GObject::Fighter * fgt = GObject::globalFighters[fighterId];
+                if(!fgt)
                     break;
-                m_fighters[i] =  BattleSimulator::CreateFighter(f->GetTypeId(),_formula,f,0,0);
+                m_fighters[i] =  BattleSimulator::CreateFighter(fgt->GetTypeId(),_formula,fgt,0,0);
                 if( m_fighters[i])
                 {
                     m_fighters[i]->setMainFighter(this);
