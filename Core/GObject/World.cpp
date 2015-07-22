192fc373ab7134dd629ab38e018559f634f7be08
libo <498714364@qq.com>
2015-07-22 17:36:48 +0800

    skillEffect 12

--- Core/GObject/World.cpp
+++ Core/GObject/World.cpp
@@ -72,7 +72,7 @@ namespace GObject
         //GMHandler::Battle(1,2);
 
         //GMHandler::Battle(10,1);
-        GMHandler::Battle(1001,1004);
+        GMHandler::Battle(1003,1002);
         return true; 
     }
 
